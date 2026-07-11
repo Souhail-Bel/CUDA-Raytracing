6773aef3a706b3623d20c6ea259b6b3035c94fc0
Souhail-Bel <robotboy976@gmail.com>
2026-07-11 20:32:15 +0100

    Added multi-sampling

--- src/renderer.cu
+++ src/renderer.cu
@@ -16,6 +16,7 @@ __constant__ Material d_materials[MAX_OBJECTS];
 __constant__ int d_num_spheres;
 
 static constexpr int MAX_DEPTH = 8;
+static constexpr int NUM_SAMPLING = 16;
 
 // ABGR8888, SDL_PIXELFORMAT_RGBA32 on LE Linux/x86
 __device__ __forceinline__ uint32_t color_to_rgba(const color &c) {
@@ -69,7 +70,7 @@ __device__ color ray_color(Ray r, curandState *s) {
 }
 
 // kernel
-__global__ void render_kernel(uint32_t *fb, int width, int height,
+__global__ void render_kernel(uint32_t *fb, int width, int height, int samples,
                               curandState *rand_states) {
   const int px = threadIdx.x + blockIdx.x * blockDim.x;
   const int py = threadIdx.y + blockIdx.y * blockDim.y;
@@ -84,13 +85,15 @@ __global__ void render_kernel(uint32_t *fb, int width, int height,
 
   color pixel(0.f, 0.f, 0.f);
 
-  const float u = (float(px) + rand_f(&state)) / float(width - 1);
-  const float v = (float(py) + rand_f(&state)) / float(height - 1);
-  pixel = ray_color(d_cam.get_ray(u, v), &state);
+  for (int i = 0; i < samples; ++i) {
+    const float u = (float(px) + rand_f(&state)) / float(width - 1);
+    const float v = (float(py) + rand_f(&state)) / float(height - 1);
+    pixel += ray_color(d_cam.get_ray(u, v), &state);
+  }
 
   rand_states[p_idx] = state; // for next frame
 
-  fb[p_idx] = color_to_rgba(pixel);
+  fb[p_idx] = color_to_rgba(pixel / float(samples));
 }
 
 // called once at startup, each thread gets unique sseed
@@ -133,27 +136,32 @@ __host__ void launch_render(uint32_t *d_fb, void *d_rand_states,
   auto *states = static_cast<curandState *>(d_rand_states);
 
   // SETUP
-  const float angle = params.time * 1.f;
+  const float angle = params.time * 0.1f;
   const float distance = 3.f;
   const Camera cam(point3(sinf(angle) * distance, 1.f, cosf(angle) * distance),
                    point3(0.f, 0.f, 0.f), vec3(0.f, 1.f, 0.f), 90.f,
                    params.aspect_ratio);
   CUDA_CHECK(cudaMemcpyToSymbol(d_cam, &cam, sizeof(Camera)));
 
-  const int num_spheres = 4;
-  const int num_materials = 4;
+  const int num_spheres = 6;
+  const int num_materials = 5;
 
   const Material mats[num_materials] = {
       {color(0.8f, 0.8f, 0.f), 0.f, 0.f, MatType::Lambertian},
       {color(0.3f, 0.3f, 0.3f), 0.f, 0.f, MatType::Metal},
       {color(1.f, 1.f, 1.f), 0.f, 1.5f, MatType::Dielectric},
       {color(0.2f, 0.6f, 0.2f), 0.f, 0.f, MatType::Lambertian},
+      {color(0.1f, 0.1f, 0.4f), 0.5f, 0.f, MatType::Metal},
   };
 
-  const Sphere spheres[num_spheres]{Sphere(point3(0.f, 0.f, 0.f), 0.5f, 0),
-                                    Sphere(point3(0.f, -100.5f, 0.f), 100.f, 1),
-                                    Sphere(point3(10.f, 0.f, 0.f), 4.f, 2),
-                                    Sphere(point3(0.f, 2.f, 0.f), 1.f, 3)};
+  const Sphere spheres[num_spheres]{
+      Sphere(point3(0.f, 0.f, 0.f), 0.5f, 0),
+      Sphere(point3(0.f, -100.5f, 0.f), 100.f, 1),
+      Sphere(point3(-10.f, 0.f, 0.f), 4.f, 2),
+      Sphere(point3(0.f, 2.f, 0.f), 1.f, 3),
+      Sphere(point3(1.f, 1.f, 1.5f), 0.5f, 2),
+      Sphere(point3(-1.f, 1.f, 1.f), 0.75f, 4),
+  };
 
   CUDA_CHECK(
       cudaMemcpyToSymbol(d_materials, &mats, num_materials * sizeof(Material)));
@@ -168,7 +176,7 @@ __host__ void launch_render(uint32_t *d_fb, void *d_rand_states,
                        (params.height + block_size.y - 1) / block_size.y);
 
   render_kernel<<<grid_size, block_size>>>(d_fb, params.width, params.height,
-                                           states);
+                                           NUM_SAMPLING, states);
 
   // Check kernel execution
   CUDA_CHECK(cudaGetLastError());
